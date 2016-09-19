bdd5b694be8748534bd6b178b94be60a58a490f9
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2016-09-19 09:37:12 -0400

    finished pwd and date

--- bash.js
+++ bash.js
@@ -1,1 +1,16 @@
-console.log(Object.keys(process))
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', function(data) {
+  var cmd = data.toString().trim();
+
+  if (cmd === 'pwd') {
+    process.stdout.write(process.cwd());
+  }
+  if(cmd === 'date') {
+    var date = new Date();
+    process.stdout.write(date.toString());
+  }
+
+  // process.stdout.write('You typed ' + cmd);
+  process.stdout.write('\nprompt > ')
+})
