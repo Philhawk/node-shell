341c9b6dc111f0e8f53d47edea0721a814654d88
Philhawk <pkjacob@gmail.com>
2016-09-19 10:05:24 -0400

    base case refactored into functions

--- bash.js
+++ bash.js
@@ -1,15 +1,13 @@
+var commands = require('./command.js');
+
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', function(data) {
-  var cmd = data.toString().trim();
+  process.stdin.on('data', function(data) {
+    var cmd = data.toString().trim();
 
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-  }
-  if(cmd === 'date') {
-    var date = new Date();
-    process.stdout.write(date.toString());
-  }
+    commands.pwdFunction(cmd);
+
+  commands.dateFunction(cmd);
 
   // process.stdout.write('You typed ' + cmd);
   process.stdout.write('\nprompt > ')
